e95feebe8d8a750152a906945810a72ce0200a80
gy <b869140512@gmail.com>
2021-08-22 15:11:16 +0800

    testequals

--- src/testequals.java
+++ src/testequals.java
@@ -2,7 +2,7 @@ public class testequals {
 
     public static void main(String[] args) {
         String a="123aaa";
-        String b="123aaa";
+        String b="123aaa1";
         boolean abc;
         abc = a.equals(b);
         if(a.equals(b)){
